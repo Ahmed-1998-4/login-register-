c6e34de40851e22d27f3ed8efd7f105b3562cc68
Ahmed <ahmed123@gmail.com>
2023-12-31 15:56:16 +0200

    new files been added

---
+++ log/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Feature(models.Model):
+    username=models.CharField(max_length=50)
+    email=models.CharField(max_length=100)
